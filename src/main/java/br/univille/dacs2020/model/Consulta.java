27f51597bd682f938a853fad75b30b06dc295b0c
LOWGAN LUAN FLORES <lowgan.flores@univille.br>
2020-07-01 22:46:28 +0000

    Primeira versão

---
+++ src/main/java/br/univille/dacs2020/model/Consulta.java
@@ -0,0 +1,60 @@
+package br.univille.dacs2020.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+public class Consulta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Temporal(value = TemporalType.DATE)
+    private Date date;
+    private String status;
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name="paciente",nullable=false, foreignKey = @ForeignKey(name="fk_paciente"))
+    private Paciente paciente;
+
+    @OneToMany
+    @JoinColumn(name = "Consulta_id", foreignKey = @ForeignKey(name = "fk_consulta"))
+    private List<ProcedimentoRealizado> listaProcedimentos = new ArrayList();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(final long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(final Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(final String status) {
+        this.status = status;
+    }
+}
