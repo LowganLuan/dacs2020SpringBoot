9509a76a1c67ca6c3f341d6331bef283e0110cb2
LOWGAN LUAN FLORES <lowgan.flores@univille.br>
2020-07-02 23:39:44 +0000

    criando index

--- src/main/java/br/univille/dacs2020/service/impl/PacienteServiceImpl.java
+++ src/main/java/br/univille/dacs2020/service/impl/PacienteServiceImpl.java
@@ -3,11 +3,13 @@ package br.univille.dacs2020.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.univille.dacs2020.model.Paciente;
 import br.univille.dacs2020.repository.PacienteRepository;
 import br.univille.dacs2020.service.PacienteService;
 
+@Service
 public class PacienteServiceImpl implements PacienteService {
 
     @Autowired
