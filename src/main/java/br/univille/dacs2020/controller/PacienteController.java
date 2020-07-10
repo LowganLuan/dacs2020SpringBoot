d5d50a6cd58b39775e5766007fb8cf2db1f686a9
LOWGAN LUAN FLORES <lowgan.flores@univille.br>
2020-07-10 00:13:23 +0000

    Final

--- src/main/java/br/univille/dacs2020/controller/PacienteController.java
+++ src/main/java/br/univille/dacs2020/controller/PacienteController.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import br.univille.dacs2020.model.Paciente;
 import br.univille.dacs2020.service.PacienteService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 
 @Controller
@@ -27,5 +32,20 @@ public class PacienteController {
         return new ModelAndView("paciente/index","listapacientes",listaPacientes);
     }
 
+    @GetMapping("/novo")
+    public ModelAndView createForm(@ModelAttribute Paciente paciente) {
+        return new ModelAndView("paciente/form");
+    }
+
+    @GetMapping(value = "/alterar/{id}")
+    public ModelAndView edit(@PathVariable("id") Paciente paciente) {
+        return new ModelAndView("paciente/form","paciente",paciente);
+    }
+
+    @PostMapping(params = "form")
+    public ModelAndView save(Paciente paciente){
+        service.save(paciente);
+        return new ModelAndView("redirect:/paciente");
+    }
     
 }
